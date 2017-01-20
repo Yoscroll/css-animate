8e63562422d1f8ab8ff5e47aff8df1e70f4b1d90
Yoscroll <ianhenry.c808@gmail.com>
2017-01-20 09:20:39 -1000

    sfa

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
-function shrink() {
+console.log("k");
+function hey() {
 	var box = document.getElementById('js');
-	var id = setInterval()
+	var id = setInterval(shrink,2);
+  var num = 0;
+  function shrink(){
+    if (num ==1000){
+      clearInterval(id);
+    }
+    else{
+      num++;
+      box.style.marginLeft=num+"px";
+    }
+  }
+ console.log("hi");
 }
